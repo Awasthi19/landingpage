852cc7a8a3d1bd01ad4da918148d20e6569283b6
Awasthi19 <sushilawasthi2999@gmail.com>
2026-01-18 15:13:10 +0545

    added meta and title for SEO

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export', // Static HTML export
   compiler: {
     removeConsole: {
       exclude: ['error', 'warn'], // Remove all console except error and warn
